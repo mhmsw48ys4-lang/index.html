9b3335f828fa198930c1d4c36869557bb78996e1
mhmsw48ys4-lang <mhmsw48ys4@privaterelay.appleid.com>
2026-09-26 11:32:14 +0300

    إرجاع حد القيمة السوقية إلى 10 ملايين

--- netlify/functions/pivot.js
+++ netlify/functions/pivot.js
@@ -10,8 +10,7 @@ exports.handler = async (event) => {
 
   try {
     // نستخدم Nasdaq Screener كمصدر لقائمة الأسهم بدلاً من Yahoo Screener.
-    // الهدف: أسهم أمريكية بسعر $1-$7 ضمن نطاق small-cap واسع.
-    // لا نحصر القائمة بقيمة سوقية شديدة الانخفاض حتى لا تختفي معظم الأسهم المرشحة للاستراتيجية.
+    // الهدف: أسهم أمريكية بسعر $1-$7 وقيمة سوقية <= $10M.
     // نأخذ Nasdaq + NYSE + AMEX ثم نفلتر محلياً، وبعدها index.html يفحص القاع والشروط الفنية.
     const exchanges = ["NASDAQ", "NYSE", "AMEX"];
 
@@ -73,7 +72,7 @@ exports.handler = async (event) => {
           price <= 7 &&
           Number.isFinite(marketCap) &&
           marketCap > 0 &&
-          marketCap <= 250000000
+          marketCap <= 10000000
         ) {
           map.set(symbol, q);
         }
